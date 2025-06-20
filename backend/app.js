cbb66a617f0feedb2e7404d783a4e7968e5063b1
dhairyaveers1 <dhairyaveers1@gmail.com>
2025-06-20 21:35:52 +0530

    .env

---
+++ backend/app.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose');
+require('dotenv').config();
+
+const app = express();
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+// Connect to MongoDB
+mongoose.connect(process.env.MONGODB_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+}).then(() => console.log('MongoDB connected'))
+  .catch(err => console.error('MongoDB connection error:', err));
+
+// Export app for server.js
+module.exports = app;
